dc42cb42de0eaff44a71323186a9f5bb1950c1a6
bejjkka <go34jex@mytum.de>
2026-03-23 19:50:30 +0100

    Update player.js

--- player.js
+++ player.js
@@ -6,8 +6,8 @@ export default class Player{
     jumpPressed = false;
     jumpInProgress = false;
     falling = false;
-    JUMP_SPEED = 0.5;
-    GRAVITY = 0.4;
+    JUMP_SPEED = 0.25;
+    GRAVITY = 0.2;
 
 
     constructor(ctx, width, height, minJumpHeight, maxJumpHeight, scaleRatio){
