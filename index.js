c1669d46b07c55f2392e9fe5c8a01aa05f5eb0b5
bejjkka <go34jex@mytum.de>
2026-03-23 23:11:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ function showGameOver(){
     const smallfontSize = 15* scaleRatio;
     ctx.font= `${smallfontSize}px 'Press Start 2P'`;
     ctx.fillStyle = "white";    
-    ctx.fillText("*to retry: space/screen tap", canvas.width/2, canvas.height/2 + 60 * scaleRatio);
+    ctx.fillText("*to retry: press space/screen tap", canvas.width/2, canvas.height/2 + 60 * scaleRatio);
     }
 
 function setupReset(){
